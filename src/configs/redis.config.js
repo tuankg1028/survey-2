1957ea4e50cce99c19d464c634d484aae2dd76ed
Xander <xander@vinova.com.sg>
2021-08-07 23:20:46 +0700

    fix

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -9,6 +9,7 @@ console.log("redis", {
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  no_ready_check: true,
   password: process.env.REDIS_PASSWORD,
   auth_pass: process.env.REDIS_PASSWORD
 });
